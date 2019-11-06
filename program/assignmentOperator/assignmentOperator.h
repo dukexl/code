48b065778a3b0cc0d3f3a9c01926feab8c0091d2
dukexl <466300686@qq.com>
2019-11-06 18:24:40 +0800

    add class

---
+++ program/assignmentOperator/assignmentOperator.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+
+class CMyString
+{
+public:
+	CMyString(char* pData = nullptr);
+	CMyString(const CMyString& str);
+	~CMyString(void);
+
+	CMyString& operator = (const CMyString& str);
+	void Print();
+
+private:
+	char* m_pData;
+};
